7608c1da8df85a698016fe866dce80da4f0af580
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 11:48:24 +0200

    New translations en_us.lang.php (Turkish)

--- modules/AOR_Conditions/language/tr_TR.lang.php
+++ modules/AOR_Conditions/language/tr_TR.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -80,9 +80,9 @@ $mod_strings = array(
   'LBL_VALUE_TYPE' => 'Type',
   'LBL_VALUE' => 'Value',
   'LBL_ORDER' => 'Order',
-  'LBL_CONDITION_OPERATOR' => 'Condition Operator',
   'LBL_AOR_REPORT_ID' => 'Report Id',
   'LBL_PARAMETER' => 'Parameter',
     'LBL_CONDITION_AND' => 'AND',
     'LBL_CONDITION_OR' => 'OR',
+    'LBL_PARENTHESIS' => 'Parenthesis',
 );
