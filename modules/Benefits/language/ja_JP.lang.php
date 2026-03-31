0dc6f8a887f93e8aa10a588a4baca2044ffdfe08
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:02:32 +0200

    New translations en_us.lang.php (Japanese)

--- modules/Benefits/language/ja_JP.lang.php
+++ modules/Benefits/language/ja_JP.lang.php
@@ -7,10 +7,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -71,14 +71,13 @@ $mod_strings = array(
    'LBL_HOMEPAGE_TITLE' => 'My Benefits',
    'LNK_NEW_RECORD' => 'Create Benefit',
    'LNK_LIST' => 'View Benefits',
-   'LNK_IMPORT_BENEFITS' => 'Import Benefits',
    'LBL_SEARCH_FORM_TITLE' => 'Search Benefits',
    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-   'LBL_BENEFITS_SUBPANEL_TITLE' => 'Benefits',
    'LBL_NEW_FORM_TITLE' => 'New Benefit',
    'LBL_EDITVIEW_PANEL1' => 'OTHER',
    'LBL_ROLES' => 'Roles',
    'LBL_POSITIONS' => 'Positions',
-   'LBL_EMPLOYEES' => 'Employees'
+   'LBL_EMPLOYEES' => 'Employees',
+   'LBL_REQUESTS' => 'Requests',
 );
