abd6a073c8f32faf65b1bb81d96cc5b9218715b3
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 13:16:52 +0200

    New translations en_us.lang.php (Polish)

--- modules/SurveyQuestionOptions/language/pl_PL.lang.php
+++ modules/SurveyQuestionOptions/language/pl_PL.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -76,4 +76,5 @@ $mod_strings = array(
     'LBL_SURVEYQUESTIONOPTIONS_SUBPANEL_TITLE'                                             => 'Survey Question Options',
     'LBL_NEW_FORM_TITLE'                                                                   => 'New Survey Question Options',
     'LBL_SURVEYQUESTIONOPTIONS_SURVEYQUESTIONRESPONSES_FROM_SURVEYQUESTIONRESPONSES_TITLE' => 'Survey Question Responses',
+    'LBL_SORT_ORDER'                                                                       => 'Sort Order',
 );
