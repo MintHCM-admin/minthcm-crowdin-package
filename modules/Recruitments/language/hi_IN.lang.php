d2c0de9528f332951e11b5b7c0a6b28082cffa97
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 13:02:58 +0200

    New translations en_us.lang.php (Hindi)

--- modules/Recruitments/language/hi_IN.lang.php
+++ modules/Recruitments/language/hi_IN.lang.php
@@ -7,10 +7,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -57,19 +57,14 @@
 $mod_strings = array(
    'LBL_TEAM' => 'Teams',
    'LBL_TEAMS' => 'Teams',
-   'LBL_TEAM_ID' => 'Team Id',
    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
    'LBL_ID' => 'ID',
    'LBL_DATE_ENTERED' => 'Date Created',
    'LBL_DATE_MODIFIED' => 'Date Modified',
    'LBL_MODIFIED' => 'Modified by',
-   'LBL_MODIFIED_ID' => 'Modified by Id',
    'LBL_MODIFIED_NAME' => 'Modified by Name',
    'LBL_CREATED' => 'Created by',
-   'LBL_CREATED_ID' => 'Created by Id',
-   'LBL_DOC_OWNER' => 'Document Onwer',
-   'LBL_USER_FAVORITES' => 'Users Who Favorite',
    'LBL_DELETED' => 'Deleted',
    'LBL_NAME' => 'Name',
    'LBL_CREATED_USER' => 'Created by User',
@@ -80,19 +75,15 @@ $mod_strings = array(
    'LBL_LIST_FORM_TITLE' => 'Recruitments List',
    'LBL_MODULE_NAME' => 'Recruitments',
    'LBL_MODULE_TITLE' => 'Recruitments',
-   'LBL_MODULE_NAME_SINGULAR' => 'Recruitment',
    'LBL_HOMEPAGE_TITLE' => 'My Recruitments',
    'LNK_NEW_RECORD' => 'Create Recruitment',
    'LNK_LIST' => 'View Recruitments',
-   'LNK_IMPORT_RECRUITMENT' => 'Import Recruitments',
    'LBL_SEARCH_FORM_TITLE' => 'Search Recruitments',
    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activity Stream',
-   'LBL_RECRUITMENT_SUBPANEL_TITLE' => 'Recruitments',
    'LBL_NEW_FORM_TITLE' => 'New Recruitment',
    'LBL_IMPORT' => 'Import Recruitments',
    'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new Recruitment record by importing a vCard from your file system.',
-   'LNK_IMPORT_PROJECTS' => 'Import Recruitments',
    'LBL_PROJECTS_SUBPANEL_TITLE' => 'Recruitments',
    'LBL_RECRUITMENTS_POSITIONS_FROM_POSITIONS_TITLE' => 'Positions',
    'LBL_RECRUITMENTS_POSITIONS_FROM_RECRUITMENTS_TITLE_ID' => 'Positions ID',
@@ -104,19 +95,12 @@ $mod_strings = array(
    'LBL_START_DATE' => 'Start Date',
    'LBL_START_WORK_DATE' => 'Start Work Date',
    'LBL_END_DATE' => 'End Date',
-   'LBL_ANNOUNCEMENT' => 'Announcement',
+   'LBL_ANNOUNCEMENT' => 'News',
    'LBL_PROJECT_STATUS' => 'Status',
    'LBL_RECORDVIEW_PANEL1' => 'Main Panel',
    'LBL_DESCRIPTION' => 'Project Description',
    'LBL_VACANCY' => 'Vacancy Amount',
-   'LNK_IMPORT_RECRUITMENTS' => 'Import Recruitments',
-   'LNK_IMPORT_HR_REKRUTACJE' => 'Import Recruitments',
-   'LBL_RECRUITMENTS_CALLS_FROM_CALLS_TITLE' => 'Calls',
-   'LBL_RECRUITMENTS_TASKS_FROM_TASKS_TITLE' => 'Tasks',
-   'LBL_RECRUITMENTS_MEETINGS_FROM_MEETINGS_TITLE' => 'Meetings',
-   'LBL_RECRUITMENTS_NOTES_FROM_NOTES_TITLE' => 'Notes',
    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-   'LBL_RECRUITMENTS_EMAILS_FROM_EMAILS_TITLE' => 'E-mails',
    'LBL_VERSION' => 'Version Number',
    'LBL_RECRUITMENT_CHANNELS' => 'Recruitment Channels',
    'LBL_RECRUITMENT_TYPE' => 'Recruitment Type',
@@ -130,7 +114,7 @@ $mod_strings = array(
    'LBL_SALARY_FROM_USDOLLAR' => 'Salary From (base currency)',
    'LBL_SALARY_TO_USDOLLAR' => 'Salary To (base currency)',
    'LBL_CURRENCY_ID' => 'Currency (Id)',
-   'LBL_CURRENCY_RATE' => 'Currency Rate',
    'LBL_SHOW_MORE_INFORMATION' => 'Other',
    'LBL_RECRUITMENTS_ACTIVITIES_EMAILS_FROM_EMAILS_TITLE' => 'E-mails',
+   'LBL_LIST_TITLE' => 'Recruitments',
 );
