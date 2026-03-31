e22e5f639b974c783c162b10731d9fdac01418c6
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:18:20 +0200

    New translations en_us.lang.php (Portuguese, Brazilian)

--- modules/Costs/language/pt_BR.lang.php
+++ modules/Costs/language/pt_BR.lang.php
@@ -7,10 +7,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -86,10 +86,8 @@ $mod_strings = array(
    'LBL_DATE_ENTERED' => 'Date Created',
    'LBL_DATE_MODIFIED' => 'Date Modified',
    'LBL_MODIFIED' => 'Modified by',
-   'LBL_MODIFIED_ID' => 'Modified by Id',
    'LBL_MODIFIED_NAME' => 'Modified by Name',
    'LBL_CREATED' => 'Created by',
-   'LBL_CREATED_ID' => 'Created by Id',
    'LBL_DESCRIPTION' => 'Description',
    'LBL_DELETED' => 'Deleted',
    'LBL_NAME' => 'Name',
@@ -102,11 +100,9 @@ $mod_strings = array(
    'LBL_HOMEPAGE_TITLE' => 'My Costs',
    'LNK_NEW_RECORD' => 'Create Cost',
    'LNK_LIST' => 'Costs',
-   'LNK_IMPORT_COSTS' => 'Import Costs',
    'LBL_SEARCH_FORM_TITLE' => 'Costs',
    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-   'LBL_COSTS_SUBPANEL_TITLE' => 'Costs',
    'LBL_NEW_FORM_TITLE' => 'New Costs',
    'LBL_TYPE' => 'Type',
    'LBL_COST_AMOUNT' => 'Amount',
