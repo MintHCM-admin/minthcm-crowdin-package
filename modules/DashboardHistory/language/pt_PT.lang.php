f2b1e5707bfe85346dd93a8b9ee9c4c92e1fc6be
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:20:39 +0200

    New translations en_us.lang.php (Portuguese)

--- modules/DashboardHistory/language/pt_PT.lang.php
+++ modules/DashboardHistory/language/pt_PT.lang.php
@@ -6,10 +6,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -76,11 +76,9 @@ $mod_strings = array(
     'LBL_HOMEPAGE_TITLE' => 'My Dashboards Deployment History',
     'LNK_NEW_RECORD' => 'Create Dashboards Deployment History',
     'LNK_LIST' => 'View Dashboards Deployment History',
-    'LNK_IMPORT_DASHBOARDHISTORY' => 'Importuj Dashboards Deployment History',
     'LBL_SEARCH_FORM_TITLE' => 'Search Dashboards Deployment History',
     'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-    'LBL_DASHBOARDHISTORY_SUBPANEL_TITLE' => 'Dashboards Deployment History',
     'LBL_NEW_FORM_TITLE' => 'New Dashboards Deployment History',
     'LBL_DASHBOARDMANAGER' => 'Dashboard Templates',
     'LBL_DASHBOARDMANAGER_NAME' => 'Dashboard Template',
