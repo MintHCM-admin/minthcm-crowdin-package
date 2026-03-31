5a1594abfc07e6e4fdabdf95e223740f10f62c90
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:24:12 +0200

    New translations en_us.lang.php (Arabic)

--- modules/Dictionaries/language/ar_SA.lang.php
+++ modules/Dictionaries/language/ar_SA.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -63,7 +63,6 @@ $mod_strings = array(
     'LBL_MODULE_TITLE' => 'Dictionaries',
     'LNK_NEW_RECORD' => 'Create Dictionary',
     'LNK_LIST' => 'View Dictionaries',
-    'LNK_IMPORT_EXITINTERVIEWS' => 'Import Dictionaries',
     'LBL_SEARCH_FORM_TITLE' => 'Search Dictionaries',
     'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
     'LBL_NEW_FORM_TITLE' => 'New Dictionary',
