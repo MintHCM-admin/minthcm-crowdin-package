70d56bcb0c622741fb4b3786299d7c1644aaaa55
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 11:53:30 +0200

    New translations en_us.lang.php (Japanese)

--- modules/AOW_WorkFlow/language/ja_JP.lang.php
+++ modules/AOW_WorkFlow/language/ja_JP.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -80,5 +80,6 @@ $mod_strings = array(
     'LBL_ACTION_LINES' => 'Actions',
     'LBL_ADD_ACTION' => 'Add Action',
     'LBL_MULTIPLE_RUNS' => 'Repeated Runs',
-    'LBL_RUN_WHEN' => 'Run'
+    'LBL_RUN_WHEN' => 'Run',
+    'LBL_RUN_ON_IMPORT' => 'Run on Import'
 );
