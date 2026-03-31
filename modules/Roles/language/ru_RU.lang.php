f4c120e9fcc8adee4bc2049f04be4e9504e24110
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 13:08:01 +0200

    New translations en_us.lang.php (Russian)

--- modules/Roles/language/ru_RU.lang.php
+++ modules/Roles/language/ru_RU.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2011 - 2019 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -47,19 +47,19 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $mod_strings = array(
-    'LBL_ROLE' => 'Role: ',
-    'LBL_LANGUAGE' => 'Language: ',
+    'LBL_ROLE' => 'Role:',
+    'LBL_LANGUAGE' => 'Language:',
     'LBL_MODULE_NAME' => 'Roles',
     'LBL_MODULE_TITLE' => 'Roles: Home',
     'LBL_SEARCH_FORM_TITLE' => 'Role Search',
     'LBL_LIST_FORM_TITLE' => 'Role List',
     'LNK_NEW_ROLE' => 'Create Role',
     'LNK_ROLES' => 'Roles',
-    'LBL_NAME' => 'Name: ',
-    'LBL_DESCRIPTION' => 'Description: ',
-    'LBL_ALLOWED_MODULES' => 'Allowed Modules: ',
-    'LBL_DISALLOWED_MODULES' => 'Disallowed Modules: ',
-    'LBL_ASSIGN_MODULES' => 'Edit Modules: ',
+    'LBL_NAME' => 'Name:',
+    'LBL_DESCRIPTION' => 'Description:',
+    'LBL_ALLOWED_MODULES' => 'Allowed Modules:',
+    'LBL_DISALLOWED_MODULES' => 'Disallowed Modules:',
+    'LBL_ASSIGN_MODULES' => 'Edit Modules:',
     'LBL_DEFAULT_SUBPANEL_TITLE' => 'Roles',
     'LBL_USERS' => 'Users',
 
