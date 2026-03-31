51dade809e16ec00d63f0a859843040d67530674
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 11:51:50 +0200

    New translations en_us.lang.php (Serbian (Cyrillic))

--- modules/AOW_Actions/language/sr_SP.lang.php
+++ modules/AOW_Actions/language/sr_SP.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -51,13 +51,11 @@ $mod_strings = array(
     'LBL_DATE_ENTERED' => 'Date Created',
     'LBL_DATE_MODIFIED' => 'Date Modified',
     'LBL_MODIFIED' => 'Modified By',
-    'LBL_MODIFIED_ID' => 'Modified By Id',
     'LBL_MODIFIED_NAME' => 'Modified By Name',
     'LBL_CREATED_USER' => 'Created by User',
     'LBL_MODIFIED_USER' => 'Modified by User',
     'LBL_CREATED' => 'Created By',
     'LBL_DESCRIPTION' => 'Description',
-    'LBL_CREATED_ID' => 'Created By Id',
     'LBL_DELETED' => 'Deleted',
     'LBL_NAME' => 'Name',
     'LBL_MODULE_NAME' => 'WorkFlow Actions',
@@ -68,30 +66,13 @@ $mod_strings = array(
     'LBL_SELECT_ACTION' => 'Select Action',
     'LBL_CREATE_EMAIL_TEMPLATE' => 'Create',
     'LBL_RECORD_TYPE' => 'Record Type',
-    'LBL_SENDEMAIL' => 'Send Email',
-    'LBL_CREATERECORD' => 'Create Record',
-    'LBL_MODIFYRECORD' => 'Modify Record',
     'LBL_ADD_FIELD' => 'Add Field',
     'LBL_ADD_RELATIONSHIP' => 'Add Relationship',
     'LBL_EDIT_EMAIL_TEMPLATE' => 'Edit',
     'LBL_EMAIL' => 'Email',
     'LBL_EMAIL_TEMPLATE' => 'Email Template',
-    'LBL_SETAPPROVAL' => 'Set Approval',
     'LBL_RELATE_WORKFLOW' => 'Relate to WorkFlow Module',
     'LBL_INDIVIDUAL_EMAILS' => 'Send Individual Emails',
-    'LBL_COMPUTEFIELD' => 'Calculate Fields',
-    'LBL_COMPUTEFIELD_PARAMETERS' => 'Parameters',
-    'LBL_COMPUTEFIELD_FIELD_NAME' => 'Field name',
-    'LBL_COMPUTEFIELD_IDENTIFIER' => 'Identifier',
-    'LBL_COMPUTEFIELD_ADD_PARAMETER' => 'Add parameter',
-    'LBL_COMPUTEFIELD_RELATION_PARAMETERS' => 'Relation parameters',
-    'LBL_COMPUTEFIELD_RELATION' => 'Relation',
-    'LBL_COMPUTEFIELD_ADD_RELATION_PARAMETER' => 'Add relation parameter',
-    'LBL_COMPUTEFIELD_FORMULA' => 'Formula',
-    'LBL_COMPUTEFIELD_FORMULAS' => 'Formulas',
-    'LBL_COMPUTEFIELD_ADD_FORMULA' => 'Add formula',
-    'LBL_COMPUTEFIELD_VALUE_TYPE' => 'Value type',
-    'LBL_COMPUTEFIELD_RAW_VALUE' => 'Raw value',
-    'LBL_COMPUTEFIELD_FORMATTED_VALUE' => 'Formatted value',
-    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Copy emails from WorkFlow Module'
+    'LBL_COPY_EMAIL_ADDRESSES_WORKFLOW' => 'Copy emails from WorkFlow Module',
+    'LBL_ORDER' => 'Action Order',
 );
