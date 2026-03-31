c5291f702ba2f8893c24ee29fb2f40b0d6bc1ddf
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:34:53 +0200

    New translations en_us.lang.php (Danish)

--- modules/ExitInterviews/language/da_DK.lang.php
+++ modules/ExitInterviews/language/da_DK.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2025 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2025 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -72,11 +72,9 @@ $mod_strings = array(
     'LBL_HOMEPAGE_TITLE' => 'My Exit Interviews',
     'LNK_NEW_RECORD' => 'Create Exit Interview',
     'LNK_LIST' => 'View Exit Interviews',
-    'LNK_IMPORT_EXITINTERVIEWS' => 'Import Exit Interviews',
     'LBL_SEARCH_FORM_TITLE' => 'Search Exit Interviews',
     'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-    'LBL_EXITINTERVIEWS_SUBPANEL_TITLE' => 'Exit Interviews',
     'LBL_NEW_FORM_TITLE' => 'New Exit Interview',
     'LBL_DATE_START' => 'Start Date',
     'LBL_DATE_END' => 'End Date',
