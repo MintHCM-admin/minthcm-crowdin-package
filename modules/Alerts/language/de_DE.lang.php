8e73159348945b8511acbda031b76d1cb7e68e70
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 11:56:02 +0200

    New translations en_us.lang.php (German)

--- modules/Alerts/language/de_DE.lang.php
+++ modules/Alerts/language/de_DE.lang.php
@@ -9,7 +9,7 @@
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -76,4 +76,5 @@ $mod_strings = array(
    'LBL_IS_READ' => 'Is Read',
    'LBL_TYPE' => 'Type',
    'LBL_ALERT_TYPE' => 'Alert Type',
+   'LBL_IS_CLOSED' => 'Is Closed',
 );
