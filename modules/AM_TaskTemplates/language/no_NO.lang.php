8623c6db5daf74f0c6394ee92bad71bd73a8be21
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 11:43:22 +0200

    New translations en_us.lang.php (Norwegian)

--- modules/AM_TaskTemplates/language/no_NO.lang.php
+++ modules/AM_TaskTemplates/language/no_NO.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2025 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -53,10 +53,8 @@ $mod_strings = array(
     'LBL_DATE_ENTERED' => 'Date Created',
     'LBL_DATE_MODIFIED' => 'Date Modified',
     'LBL_MODIFIED' => 'Modified By',
-    'LBL_MODIFIED_ID' => 'Modified By Id',
     'LBL_MODIFIED_NAME' => 'Modified By Name',
     'LBL_CREATED' => 'Created By',
-    'LBL_CREATED_ID' => 'Created By Id',
     'LBL_DESCRIPTION' => 'Description',
     'LBL_DELETED' => 'Deleted',
     'LBL_NAME' => 'Task Name',
@@ -71,11 +69,9 @@ $mod_strings = array(
     'LBL_HOMEPAGE_TITLE' => 'My Project Task Templates',
     'LNK_NEW_RECORD' => 'Create Project Task Templates',
     'LNK_LIST' => 'View Project Task Templates',
-    'LNK_IMPORT_AM_TASKTEMPLATES' => 'Import Project Task Templates',
     'LBL_SEARCH_FORM_TITLE' => 'Search Project Task Templates',
     'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-    'LBL_AM_TASKTEMPLATES_SUBPANEL_TITLE' => 'Project Task Templates',
     'LBL_NEW_FORM_TITLE' => 'New Project Task Templates',
     'LBL_STATUS' => 'Status',
     'LBL_PRIORITY' => 'Priority',
