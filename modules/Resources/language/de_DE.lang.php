13aaca5a8721db65efe02627fe9845c6f8d27147
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 13:05:58 +0200

    New translations en_us.lang.php (German)

--- modules/Resources/language/de_DE.lang.php
+++ modules/Resources/language/de_DE.lang.php
@@ -7,10 +7,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2025 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2025 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -56,10 +56,8 @@ $mod_strings = array(
    'LBL_DATE_ENTERED' => 'Date Created',
    'LBL_DATE_MODIFIED' => 'Date Modified',
    'LBL_MODIFIED' => 'Modified by',
-   'LBL_MODIFIED_ID' => 'Modifiers ID',
    'LBL_MODIFIED_NAME' => 'Modified by Name',
    'LBL_CREATED' => 'Created by',
-   'LBL_CREATED_ID' => 'Creator ID',
    'LBL_DESCRIPTION' => 'Description',
    'LBL_DELETED' => 'Deleted',
    'LBL_NAME' => 'Name',
@@ -80,7 +78,6 @@ $mod_strings = array(
    'LBL_SEARCH_FORM_TITLE' => 'Search Resources',
    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-   'LBL_RESOURCES_SUBPANEL_TITLE' => 'Resources',
    'LBL_NEW_FORM_TITLE' => 'New Resources',
    'LBL_TYPE' => 'Type',
    'LBL_UNAVAILABLE' => 'Unavailable',
@@ -93,4 +90,5 @@ $mod_strings = array(
    'LBL_ROOM_NAME' => 'Room',
    'LBL_ROOM_ID' => 'Room ID',
    'LBL_ROOMS_RESOURCES_TITLE' => 'Rooms',
+   'LBL_FILES' => 'Files',
 );
