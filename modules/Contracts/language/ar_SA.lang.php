f75ec0ba10782bc20a9f2156e102afdeadedeab9
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:17:00 +0200

    New translations en_us.lang.php (Arabic)

--- modules/Contracts/language/ar_SA.lang.php
+++ modules/Contracts/language/ar_SA.lang.php
@@ -5,10 +5,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2023 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -75,11 +75,9 @@ $mod_strings = array(
     'LBL_HOMEPAGE_TITLE' => 'My Contracts',
     'LNK_NEW_RECORD' => 'Create Contract',
     'LNK_LIST' => 'View Contracts',
-    'LNK_IMPORT_CONTRACTS' => 'Import Contracts',
     'LBL_SEARCH_FORM_TITLE' => 'Search Contracts',
     'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
     'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-    'LBL_CONTRACTS_SUBPANEL_TITLE' => 'Contracts',
     'LBL_NEW_FORM_TITLE' => 'New Contract',
     'LBL_DATE_OF_SIGNING' => 'Date Of Signing',
     'LBL_CONTRACT_STARTING_DATE' => 'Contract Starting Date',
