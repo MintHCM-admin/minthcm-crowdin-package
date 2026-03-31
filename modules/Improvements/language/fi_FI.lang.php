8be964f87f008f4a89e94cb12d551d86969a7678
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:44:21 +0200

    New translations en_us.lang.php (Finnish)

--- modules/Improvements/language/fi_FI.lang.php
+++ modules/Improvements/language/fi_FI.lang.php
@@ -7,10 +7,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2025 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2025 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -71,7 +71,6 @@ $mod_strings = array(
    'LBL_HOMEPAGE_TITLE' => 'My Improvements',
    'LNK_NEW_RECORD' => 'Create Improvement',
    'LNK_LIST' => 'View Improvements',
-   'LNK_IMPORT_IMPROVEMENTS' => 'Import Improvements',
    'LBL_SEARCH_FORM_TITLE' => 'Search Improvements',
    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
    'LBL_NEW_FORM_TITLE' => 'New Improvement',
