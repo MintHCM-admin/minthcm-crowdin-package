01379ecd9281c933bb0ac51c4bc8a7d6ce5f081b
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 13:21:02 +0200

    New translations en_us.lang.php (Spanish)

--- modules/TermsOfEmployment/language/es_ES.lang.php
+++ modules/TermsOfEmployment/language/es_ES.lang.php
@@ -8,7 +8,7 @@
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2023 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -96,6 +96,6 @@ $mod_strings = array(
     'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Organizational Unit',
     'LBL_DOCUMENTS' => 'Documents',
     'LBL_POSITION_NAME' => 'Position',
-
     'LBL_CONTRACTED_EMPLOYEE' => 'Contracted Employee',
+    'LBL_POSITION_ID' => 'Position (ID)',
 );
