ab4ff16c2463d5de16f980b21ed9bc09a92120e5
MintHCM <51930758+MintHCM-admin@users.noreply.github.com>
2026-03-31 12:54:55 +0200

    New translations en_us.lang.php (Dutch)

--- modules/OnboardingTemplates/language/nl_NL.lang.php
+++ modules/OnboardingTemplates/language/nl_NL.lang.php
@@ -6,10 +6,10 @@
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ * Copyright (C) 2018-2025 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
+ * Copyright (C) 2018-2025 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -73,15 +73,11 @@ $mod_strings = array(
    'LBL_HOMEPAGE_TITLE' => 'My Onboarding Templates',
    'LNK_NEW_RECORD' => 'Create Onboarding Template',
    'LNK_LIST' => 'View Onboarding Templates',
-   'LNK_IMPORT_ONBOARDINGTEMPLATES' => 'Import Onboarding Templates',
    'LBL_SEARCH_FORM_TITLE' => 'Search Onboarding Templates',
    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-   'LBL_ONBOARDINGTEMPLATES_SUBPANEL_TITLE' => 'Onboarding Templates',
    'LBL_NEW_FORM_TITLE' => 'New Onboarding Template',
    'LBL_ONBOARDINGTEMPLATES_POSITIONS_TITLE' => 'Positions',
-   'LBL_POSITION_NAME' => 'Position',
-   'LBL_POSITION_ID' => 'Position (ID)',
    'LBL_ONBOARDINGS_ONBOARDINGTEMPLATES_TITLE' => 'Onboardings',
    'LBL_GENERATE_BUTTON' => 'Generate Onboarding',
    'LBL_ONBOARDINGOFFBOARDINGELEMENTS' => 'Onboarding/Offboarding Elements',
